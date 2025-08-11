a795d5a0d3cb6a6f22d8b981867fa186c112c212
smsalim931 <smsalim931@gmail.com>
2025-08-12 01:16:11 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -4,12 +4,41 @@ import React, { useState } from "react"
 const page = ()=>{
   const [title, setTitle] = useState('')
   const [task, setTask] = useState('')
+  const [mainTask, setMainTask] = useState([])
 
   const addTask = (e)=>{
     e.preventDefault()
+    setMainTask([...mainTask, {title, task}])
     console.log(title, task)
     setTitle('')
     setTask('')
+  }
+
+  const deleteHandler = (i)=>{
+    let copyTask = [...mainTask]
+    copyTask.splice(i, 1)
+    setMainTask(copyTask)
+  }
+
+  let renderTask = <h2>No Task Availble</h2>
+
+  if(mainTask.length>0){
+    renderTask = mainTask.map((t, i)=>{
+    return (
+    <li key={i} className="flex items-center justify-between">
+      <div className="flex items-center justify-between mb-5 size-2/3">
+        <h4 className="text-2xl font-semibold">{t.title}</h4>
+        <h4 className="text-2xl font-semibold">{t.task}</h4>
+      </div>
+      <button
+        onClick={()=>{
+          deleteHandler(i)
+        }}
+        className="bg-red-950 px-3 py-2 rounded text-2xl font-bold">
+        Delete
+      </button>
+    </li> )
+  })
   }
 
   return (
@@ -23,7 +52,6 @@ const page = ()=>{
         onChange={(e)=>{
           setTitle(e.target.value)
         }}/>
-        <br />
         <input type="text"
         className="text-2xl border-zinc-800 bg-white text-black border-2 m-5 px-3 py-4 rounded"
         placeholder="Enter description"
@@ -31,10 +59,15 @@ const page = ()=>{
         onChange={(e)=>{
           setTask(e.target.value)
         }}/>
-        <br />
-        <button className="bg-zinc-600 m-5 px-6 py-4 text-2xl rounded">Add Task</button>
-        
+        <button className="bg-zinc-600 m-5 px-6 py-4 text-2xl rounded">Add Task</button>        
       </form>
+      <hr className="border-t-2 mt-4"></hr>
+      <div className="p-8 bg-zinc-700 text-amber-100">
+        <ul>
+          {renderTask}
+        </ul>
+      </div>
+        
     </div>
   )
 }
